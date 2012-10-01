154a8302a9c0bc73c712924d88f383cd9a5413b7
JordiPlana <JordiPlana@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-01 08:44:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/404-simple-redirect/trunk@606441 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 404-simple-redirect.php
+++ 404-simple-redirect.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: 404 Simple Redirect
  * Plugin URI: http://jordiplana.com/404-simple-redirect-plugin-for-wordpress
- * Description: This plugin hooks the normal Wordpress workflow in order to determine if the request is processing will cause a 404 HTTP error. In that case it prevents Wordpress to do any other processing and sends the user to the page defined in the plugin options.
+ * Description: This plugin hooks the normal Wordpress workflow in order to determine if the request is processing will cause a 404 HTTP error.
  * Version: 1.2
  * Author: Jordi Plana
  * Author URI: http://jordiplana.com
